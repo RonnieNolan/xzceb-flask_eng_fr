d6d2fe4bf9d573d294acdff2cbb95babffe88709
RonnieNolan <117862054+RonnieNolan@users.noreply.github.com>
2022-11-14 18:14:22 +0200

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,16 +3,14 @@ from translator import watson_trans
 from translator import english_2_french
 from translator import french_2_english
 
-class english2french(unittest.TestCase): 
-    def test1(self): 
+class TestTranslator(unittest.TestCase): 
+    def test_english_2_french(self): 
         self.assertEqual(english_2_french("Hello"), "Bonjour")
         self.assertEqual(english_2_french("Goodbye"), "Au revoir")
         self.assertEqual(english_2_french("I'll be back"), "Je seral de retour")
         self.assertNotEqual(english_2_french("None"), " ") 
         
-
-class french2english(unittest.TestCase): 
-    def test1(self): 
+    def test_french_2_english(self): 
         self.assertEqual(french_2_english("Bonjour"), "Hello")
         self.assertEqual(french_2_english("Au revoir"), "Goodbye")
         self.assertEqual(french_2_english("Je seral de retour"), "I'll be back")
